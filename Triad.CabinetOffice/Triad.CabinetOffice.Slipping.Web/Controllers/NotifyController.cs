8e6153b591c6284bcfba36a064d74775a178e8e9
david-preston-triad <david.preston@triad.co.uk>
2017-09-11 11:13:39 +0100

    Changes for demo

--- Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/Controllers/NotifyController.cs
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/Controllers/NotifyController.cs
@@ -20,18 +20,18 @@ namespace Triad.CabinetOffice.Slipping.Web.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(FormCollection formValues)
+        public ActionResult Index(string emailAddress)
         {
             string apiKey = "slippingdev-19c65c29-9cb4-49cd-8be3-38fed8f884be-c0e20c1f-3fab-4991-bfe5-da90288a2338";
             NotificationClient client = new NotificationClient(apiKey);
-            string emailAddress = "david.preston@triad.co.uk";
+            //string emailAddress = "david.preston@triad.co.uk";
             string templateId = "b78dfef1-10fc-48f9-afa3-947cac2499d5";
             //Template missing personalisation: name, absence_date, reference
             Dictionary<string, dynamic> personalisations = new Dictionary<string, dynamic>()
             {
                 { "name", "David Preston" },
                 { "absence_date", new DateTime(2017,10,14).ToShortDateString() },
-                { "reference", "Please forward to Andrew so I can see what it looks like!" }
+                { "reference", "1234" }
             };
             EmailNotificationResponse response = client.SendEmail(emailAddress, templateId, personalisations);
             return View();
---
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/ViewModels/FromDateAndTime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Triad.CabinetOffice.Slipping.Web.ViewModels
+{
+    public class FromDateAndTime
+    {
+        public DateTime FromDate = DateTime.Now;
+        public string FromTime = "00:00";
+        public IEnumerable<SelectListItem> Times = new List<SelectListItem>()
+        {
+            new SelectListItem(){ Text="00:00", Value="00:00"},
+            new SelectListItem(){ Text="01:00", Value="01:00"},
+            new SelectListItem(){ Text="02:00", Value="02:00"},
+            new SelectListItem(){ Text="03:00", Value="03:00"},
+            new SelectListItem(){ Text="04:00", Value="04:00"},
+            new SelectListItem(){ Text="05:00", Value="05:00"},
+            new SelectListItem(){ Text="06:00", Value="06:00"},
+            new SelectListItem(){ Text="07:00", Value="07:00"},
+            new SelectListItem(){ Text="08:00", Value="08:00"},
+            new SelectListItem(){ Text="09:00", Value="09:00"},
+            new SelectListItem(){ Text="10:00", Value="10:00"},
+            new SelectListItem(){ Text="11:00", Value="11:00"},
+            new SelectListItem(){ Text="12:00", Value="12:00"},
+            new SelectListItem(){ Text="13:00", Value="13:00"},
+            new SelectListItem(){ Text="14:00", Value="14:00"},
+            new SelectListItem(){ Text="15:00", Value="15:00"},
+            new SelectListItem(){ Text="16:00", Value="16:00"},
+            new SelectListItem(){ Text="17:00", Value="17:00"},
+            new SelectListItem(){ Text="18:00", Value="18:00"},
+            new SelectListItem(){ Text="19:00", Value="19:00"},
+            new SelectListItem(){ Text="20:00", Value="20:00"},
+            new SelectListItem(){ Text="21:00", Value="21:00"},
+            new SelectListItem(){ Text="22:00", Value="22:00"},
+            new SelectListItem(){ Text="23:00", Value="23:00"}
+        };
+    }
+}
