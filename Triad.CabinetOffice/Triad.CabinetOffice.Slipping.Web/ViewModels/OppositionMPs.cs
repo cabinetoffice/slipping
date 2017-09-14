2d7c68a513a654dfc538a5c9f6d894bb6bf33f7f
andrew-lott <andrewlott.uk@gmail.com>
2017-09-14 09:57:54 +0100

    Added in govuk JS from prototype and added static links in _Layout.cshtml for now.

--- Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/ViewModels/OppositionMPs.cs
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/ViewModels/OppositionMPs.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Triad.CabinetOffice.Slipping.Web.ViewModels
 {
     public class OppositionMPs
     {
+        [Required]
         public bool? YesNo { get; set; }
         public Dictionary<int,string> MPs { get; set; }
         public OppositionMPs()
