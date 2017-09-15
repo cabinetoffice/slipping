ae1fbfa54ece7d204be285a1ed55a8b21a4c0c62
david-preston-triad <david.preston@triad.co.uk>
2017-09-15 15:46:10 +0100

    PAWS2-80: Applied review comment

--- Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/Controllers/SlippingController.cs
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/Controllers/SlippingController.cs
@@ -321,7 +321,9 @@ namespace Triad.CabinetOffice.Slipping.Web.Controllers
             if (slippingRequest != null)
             {
                 if (slippingRequest.OppositionMPs.Count == 0)
+                {
                     slippingRequest.OppositionMPs.Add(new OppositionMP() { ID = 0, MPID = 0, FullName = null });
+                }
 
                 var model = new OppositionMPs
                 {
