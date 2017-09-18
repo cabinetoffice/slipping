f58daefef7c1b4b508db8c7aa109cc97d49462bb
andrew-lott <andrewlott.uk@gmail.com>
2017-09-18 12:32:39 +0100

    Remove prototype warning.

--- Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/public/javascripts/application.js
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/public/javascripts/application.js
@@ -1,11 +1,6 @@
 /* global $ */
 /* global GOVUK */
 
-// Warn about using the kit in production
-if (window.console && window.console.info) {
-  window.console.info('GOV.UK Prototype Kit - do not use for production')
-}
-
 $(document).ready(function () {
     // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
     // with role="button" when the space key is pressed.
