c7ed292297af2b1d6d8a45012d27a9f40e26e65d
Umair Naeem <umair.naeem80@outlook.com>
2017-09-26 14:28:57 +0100

    Updated the db view

--- Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Database/dbo/Views/AbsenceRequest_AuditHistory.sql
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Database/dbo/Views/AbsenceRequest_AuditHistory.sql
@@ -1,15 +1,25 @@
-﻿CREATE VIEW [dbo].[AbsenceRequest_AuditHistory]
-	AS SELECT AR.ID,AR.MPID,AR.FromDate,AR.ToDate,
+﻿CREATE VIEW [dbo].[AbsenceRequest_AuditHistory] AS 
+SELECT AR.ID,AR.MPID,AR.FromDate,AR.ToDate,
        Case AR.ReasonID
 			When 1 Then 'Government Work (Secretaries of State / Ministers of State only)'
 			When 2 Then 'Constituency Engagement'
 			When 3 Then 'Parliamentary Campaigning Activity'
 			When 5 Then 'Personal/Other'
 	   End Reason,AR.Details,AR.[Location],AR.TravelTimeInHours,
-	   AR.PAWSAbsenceRequestID,
 	   AR.CreatedDate,U1.ForeNames + ' '+ U1.Surname [CreatedBy],AR.LastChangedDate, U2.ForeNames + ' '+ U2.Surname [LastChangeBy],
-	   ARMP.MPFullName [OppositionMPAttending]
-FROM [dbo].[AbsenceRequest] AS AR
- LEFT JOIN [dbo].[AbsenceRequestOppositionMP] AS ARMP ON AR.ID=ARMP.AbsenceRequestID
- LEFT JOIN [dbo].[User] AS U1 on U1.ID=AR.CreatedBy
- LEFT JOIN [dbo].[User] AS U2 on U2.ID=AR.LastChangedBy
+	   STRING_AGG(ARMP.MPFullName, ' , ')  AS  [OppositionMPAttending],
+	   AR.PAWSAbsenceRequestID
+FROM [dbo].[AbsenceRequest] AR
+ LEFT JOIN [dbo].[AbsenceRequestOppositionMP] ARMP ON AR.ID=ARMP.AbsenceRequestID
+ LEFT JOIN [dbo].[User] U1 on U1.ID=AR.CreatedBy
+ LEFT JOIN [dbo].[User] U2 on U2.ID=AR.LastChangedBy
+Group by AR.ID,AR.MPID,AR.FromDate,AR.ToDate,
+       Case AR.ReasonID
+			When 1 Then 'Government Work (Secretaries of State / Ministers of State only)'
+			When 2 Then 'Constituency Engagement'
+			When 3 Then 'Parliamentary Campaigning Activity'
+			When 5 Then 'Personal/Other'
+	   End,
+	   AR.Details,AR.[Location],AR.TravelTimeInHours,
+	   AR.CreatedDate,U1.ForeNames + ' '+ U1.Surname,AR.LastChangedDate, U2.ForeNames + ' '+ U2.Surname,
+	   AR.PAWSAbsenceRequestID
