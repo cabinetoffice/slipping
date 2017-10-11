eb6695fc31c21a59c73ea049438488835fcfe221
andrew-lott <andrewlott.uk@gmail.com>
2017-10-11 20:00:47 +0100

    Remove merge of paws-development and development branches.

--- Triad.CabinetOffice/Triad.CabinetOffice.PAWS.Web/src/app/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './app.component';
-export * from './login/login.component';
-export * from './logout/logout.component';
-export * from './main/main.component';
-export * from './main/home/home.component';
--- Triad.CabinetOffice/Triad.CabinetOffice.PAWS.Web/src/app/main/absence-requests/models/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './absence-request';
