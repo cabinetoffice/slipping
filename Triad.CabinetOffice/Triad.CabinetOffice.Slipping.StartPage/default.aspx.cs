5b51f3acdcb1f867428ef2dff44cc586a17f1945
andrew-lott <andrewlott.uk@gmail.com>
2017-10-13 12:29:32 +0100

    Created project for slipping start page.

---
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.StartPage/default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Triad.CabinetOffice.Slipping.StartPage
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnStart.NavigateUrl = ConfigurationManager.AppSettings["SlippingURL"];
+        }
+    }
+}
