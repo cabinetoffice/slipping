e360cd3dc7bd0d147d2e888481aaad92410db1df
Umair Naeem <umair.naeem80@outlook.com>
2017-10-23 17:50:07 +0100

    Re-structured the project of PAWS2

---
+++ Triad.CabinetOffice/Triad.CabinetOffice.PAWS.API.Tests/SessionsControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Triad.CabinetOffice.PAWS.API.Tests
+{
+    [TestClass]
+    public class SessionsControllerTest
+    {
+        [TestMethod]
+        public void GetList()
+        {
+            
+        }
+        [TestMethod]
+        public void Get()
+        {
+
+        }
+    }
+}
---
+++ Triad.CabinetOffice/Triad.CabinetOffice.PAWS.API/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triad.CabinetOffice.PAWS.API.Repositories
+{
+    interface IRepository<T>
+    {
+        IEnumerable<T> List();
+        T Get(int Id);
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
