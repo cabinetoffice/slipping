a5e47fd13e9e106cc52685f1deb056116cb926f2
david-preston-triad <david.preston@triad.co.uk>
2017-10-23 16:36:21 +0100

    Moved Welcome Page back into main Slipping application

--- Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/Controllers/HomeController.cs
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Web/Controllers/HomeController.cs
@@ -3,12 +3,12 @@ using System.Web.Mvc;
 
 namespace Triad.CabinetOffice.Slipping.Web.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            return Redirect(ConfigurationManager.AppSettings["StartPageURL"]);
+            return View();
+            //return Redirect(ConfigurationManager.AppSettings["StartPageURL"]);
         }
 
         public ActionResult NotFound()
