a46de6224512da26910fadbfb7a339230c03e4b1
andrew-lott <andrewlott.uk@gmail.com>
2017-10-24 14:35:20 +0100

    Missing from last commit due to 'Cannot read property 'resourceUri' of undefined'.

--- Triad.CabinetOffice/paws2/src/webparts/sessionForm/components/SessionForm.tsx
+++ Triad.CabinetOffice/paws2/src/webparts/sessionForm/components/SessionForm.tsx
@@ -8,20 +8,7 @@ import { TextField } from 'office-ui-fabric-react/lib/TextField';
 import { DatePicker } from 'office-ui-fabric-react/lib/DatePicker';
 import { DefaultButton, IButtonProps } from 'office-ui-fabric-react/lib/Button';
 import { HttpClient, HttpClientResponse, SPHttpClient, SPHttpClientResponse,GraphHttpClient } from '@microsoft/sp-http';
-
-export interface ISession {
-  ID:number;
-  SessionTitle:string;
-  FromDate:Date;
-  ToDate:Date;
-}
-
-export class Session implements ISession {
-  public ID:number;
-  public SessionTitle:string;
-  public FromDate:Date;
-  public ToDate:Date;
-}
+import { Session, ISession } from '../../../types/Session';
 
 export default class SessionForm extends React.Component<ISessionFormProps, ISessionFormState> {
   
@@ -29,10 +16,11 @@ export default class SessionForm extends React.Component<ISessionFormProps, ISes
     super(props);
     this.state = new SessionFormState();
     
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
     this.handleChangeSessionTitle = this.handleChangeSessionTitle.bind(this);
     this.handleChangeFromDate = this.handleChangeFromDate.bind(this);
     this.handleChangeToDate = this.handleChangeToDate.bind(this);
-    this.handleNew = this.handleNew.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
     window.addEventListener('hashchange', (e) => {
@@ -43,26 +31,30 @@ export default class SessionForm extends React.Component<ISessionFormProps, ISes
   public render(): React.ReactElement<ISessionFormProps> {
     return (
       <form className={styles.sessionForm}>
-      <div>
+        <h2>{ this.state.id === 0 ? 'Add' : this.state.viewMode ? 'View' : 'Edit' } Session</h2>
         <div>
-          <Label required={true} className={styles.msLabel}>Session Title</Label>
-          <TextField required={true} value={this.state.sessionTitle} onChanged={this.handleChangeSessionTitle}  />
+          <div>
+            <Label required={true} className={styles.msLabel}>Session Title</Label>
+            <TextField disabled={ this.state.viewMode } required={true} value={this.state.sessionTitle} onChanged={this.handleChangeSessionTitle}  />
+          </div>
+          <div>
+            <Label required={true} className={styles.msLabel}>From Date</Label>
+            <DatePicker disabled={ this.state.viewMode } value={this.state.fromDate} formatDate={(date:Date)=>date.toLocaleDateString()} onSelectDate={this.handleChangeFromDate} placeholder="Select session from date..." />
+          </div>
+          <div>
+            <Label required={true} className={styles.msLabel}>To Date</Label>
+            <DatePicker disabled={ this.state.viewMode } value={this.state.toDate} formatDate={(date:Date)=>date.toLocaleDateString()} onSelectDate={this.handleChangeToDate} placeholder="Select session end date..." />
+          </div>
+          <div>
+            { this.state.viewMode ? (
+              <DefaultButton text="Edit" className={styles.buttonPrimary} onClick={this.handleEdit} />
+            ) : (
+              <DefaultButton text="Save" className={styles.buttonPrimary} onClick={this.handleSubmit} />
+            )}
+            &nbsp;<DefaultButton text="Cancel" onClick={this.handleCancel} />
+          </div>
         </div>
-        <div>
-          <Label required={true} className={styles.msLabel}>From Date</Label>
-          <DatePicker value={this.state.fromDate} formatDate={(date:Date)=>date.toLocaleDateString()} onSelectDate={this.handleChangeFromDate} placeholder="Select session from date..." />
-        </div>
-        <div>
-          <Label required={true} className={styles.msLabel}>To Date</Label>
-          <DatePicker value={this.state.toDate} formatDate={(date:Date)=>date.toLocaleDateString()} onSelectDate={this.handleChangeToDate} placeholder="Select session end date..." />
-        </div>
-        <div>
-          <DefaultButton text="Save" className={styles.buttonPrimary} onClick={this.handleSubmit} />&nbsp;
-          <DefaultButton text="Cancel" />&nbsp;
-          <DefaultButton text="New" onClick={this.handleNew} />
-        </div>
-      </div>
-    </form>
+      </form>
     );
   }
 
@@ -74,8 +66,17 @@ export default class SessionForm extends React.Component<ISessionFormProps, ISes
         this.setState({sessionTitle:session.SessionTitle});
         this.setState({fromDate:new Date(session.FromDate)});
         this.setState({toDate:new Date(session.ToDate)});
+        this.setState({viewMode:true});
       });
+    } else {
+      this.setState(new SessionFormState());
     }
+  }
+  private handleEdit(e):void{
+    this.setState({viewMode:false});
+  }
+  private handleCancel(e):void{
+    this.componentDidMount();
   }
   private handleChangeSessionTitle(e):void{
     this.setState({sessionTitle:e});
@@ -86,18 +87,25 @@ export default class SessionForm extends React.Component<ISessionFormProps, ISes
   private handleChangeToDate(e):void{
     this.setState({toDate:e});
   }
-  private handleNew(e):void{
-    e.preventDefault();
-    this.setState(new SessionFormState());
-  }
   private handleSubmit(e):void{
     e.preventDefault();
-    this.saveSession(this.state);
+    if(this.state.id === 0){
+      this.createSession(this.state).then((session:ISession)=>{
+        this.setState({id:session.ID});
+        this.setState({sessionTitle:session.SessionTitle});
+        this.setState({fromDate:new Date(session.FromDate)});
+        this.setState({toDate:new Date(session.ToDate)});
+        this.setState({viewMode:true});
+      });
+    }else{
+      this.saveSession(this.state).then((ok:boolean)=>{ this.setState({viewMode:true}); });
+    }
   }
 
+  //#region Service Functions
   private getSession(id:number): Promise<ISession>{
     return new Promise<ISession>((resolve: (session:ISession)=>void, reject:(error:any)=>void):void=>{
-      this.props.httpClient.get(this.props.apiUrl + "/odata/Sessions(" + id + ")", HttpClient.configurations.v1, { credentials:'include' })
+      this.props.httpClient.get(`${this.props.apiUrl}(${id})`, HttpClient.configurations.v1, { credentials:'include' })
       .then((response:Response):Promise<ISession> => {
         return response.json();
       })
@@ -108,54 +116,46 @@ export default class SessionForm extends React.Component<ISessionFormProps, ISes
       });
     });
   }
-  private saveSession(session:ISessionFormState): Promise<any>{
-    if(session.id===0 ||session.id===null){
-      var postSession = {
-        'odata.metadata': this.props.apiUrl + '/odata/$metadata#Sessions/@Element',
-        CreatedBy: 1,
-        CreatedDate: new Date(),
-        FromDate: session.fromDate,
-        //ID: session.id,
-        LastChangedBy: 1,
-        LastChangedDate: new Date(),
-        SessionTitle: session.sessionTitle,
-        ToDate: session.toDate
-      };
-      return new Promise<ISession>((resolve: (session:ISession)=>void, reject:(error:any)=>void):void=>{
-        this.props.httpClient.post(this.props.apiUrl + "/odata/Sessions", HttpClient.configurations.v1, { credentials:'include', headers:{'Content-Type':'application/json'}, body:JSON.stringify(postSession) })
-        .then((response:Response):Promise<ISession> => {
-          return response.json();
-        })
-        .then((session:ISession):void=>{
-            resolve(session);
-        },(error:any):void=>{
-            reject(error);
-        });
+
+  private createSession(session:ISessionFormState):Promise<ISession>{
+    var postSession = {
+      CreatedBy: 1,
+      CreatedDate: new Date(),
+      FromDate: session.fromDate,
+      LastChangedBy: 1,
+      LastChangedDate: new Date(),
+      SessionTitle: session.sessionTitle,
+      ToDate: session.toDate
+    };
+    return new Promise<ISession>((resolve: (session:ISession)=>void, reject:(error:any)=>void):void=>{
+      this.props.httpClient.post(`${this.props.apiUrl}`, HttpClient.configurations.v1, { credentials:'include', headers:{'Content-Type':'application/json'}, body:JSON.stringify(postSession) })
+      .then((response:Response):Promise<ISession> => {
+        return response.json();
+      })
+      .then((session:ISession):void=>{
+          resolve(session);
+      },(error:any):void=>{
+          reject(error);
       });
-    } else {
-      return new Promise<boolean>((resolve: (updated:any)=>void, reject:(error:any)=>void):void=>{
-        var patchSession = {
-          CreatedBy: 1,
-          CreatedDate: new Date(),
-          FromDate: session.fromDate,
-          ID: session.id,
-          LastChangedBy: 1,
-          LastChangedDate: new Date(),
-          'odata.metadata': this.props.apiUrl + '/odata/$metadata#Sessions/@Element',
-          SessionTitle: session.sessionTitle,
-          ToDate: session.toDate
-        };
-        this.props.httpClient.fetch(this.props.apiUrl + "/odata/Sessions(" + session.id + ")", HttpClient.configurations.v1, 
-          { method:'PATCH', credentials:'include', headers:{'Content-Type':'application/json'}, body:JSON.stringify(patchSession) })
-        .then((response:Response):Promise<boolean> => {
-          return response.json();
-        })
-        .then((updated:any):void=>{
-            resolve(updated);
-        },(error:any):void=>{
-            reject(error);
-        });
-      });
-    }
+    });
   }
+
+  private saveSession(session:ISessionFormState): Promise<boolean>{
+    var patchSession = {
+      FromDate: session.fromDate,
+      ID: session.id,
+      LastChangedBy: 1,
+      LastChangedDate: new Date(),
+      SessionTitle: session.sessionTitle,
+      ToDate: session.toDate
+    };
+    return this.props.httpClient.fetch(`${this.props.apiUrl}(${session.id})`, HttpClient.configurations.v1, 
+      { method:'PATCH', credentials:'include', headers:{'Content-Type':'application/json'}, body:JSON.stringify(patchSession) })
+    .then((response:Response):boolean => {
+        return response.ok;
+    }, (error:any):void => {
+        return error;
+    });
+  }
+  //#endregion
 }
