baf1c83dd06a1f27b73d63d98d543c0a68d58be3
david-preston-triad <david.preston@triad.co.uk>
2017-10-25 17:44:11 +0100

    PAWS2-289: Reviewed table creation scripts and added two missing tables (AbsenceException and AbsenceRequestExceptions)

---
+++ Triad.CabinetOffice/Triad.CabinetOffice.Slipping.Database/dbo/Tables/AbsenceRequestExceptions.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[AbsenceRequestExceptions]
+(
+	[ID] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[AbsenceRequestID] INT NOT NULL,
+	[ExceptionID] INT NOT NULL,
+	[OppositionMPID] INT NULL,
+	[Notes] NVARCHAR(MAX) NULL,
+	[FromDate] DATETIME NOT NULL,
+	[ToDate] DATETIME NOT NULL,
+    [CreatedBy] INT NOT NULL,
+	[CreatedDate] DATETIME NOT NULL,
+	[LastChangedBy] INT NOT NULL,
+	[LastChangedDate] DATETIME NOT NULL
+	CONSTRAINT PK_AbsenceRequestExceptions PRIMARY KEY (ID)
+	CONSTRAINT FK_AbsenceRequestExceptions_AbsenceRequest FOREIGN KEY (AbsenceRequestID) REFERENCES [dbo].[AbsenceRequest](ID)
+	CONSTRAINT FK_AbsenceRequestExceptions_AbsenceException FOREIGN KEY (ExceptionID) REFERENCES [dbo].[AbsenceException](ID)
+	CONSTRAINT FK_AbsenceRequestExceptions_MembersOfParliament FOREIGN KEY (OppositionMPID) REFERENCES [dbo].[MembersOfParliament](ID)
+)
