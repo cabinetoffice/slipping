e00f4052c77abc424ea50c33a9218de85910b9ca
Umair Naeem <umair.naeem80@outlook.com>
2017-10-30 13:17:38 +0000

    Parties changes

--- Triad.CabinetOffice/paws2/src/webparts/partyForm/loc/en-us.js
+++ Triad.CabinetOffice/paws2/src/webparts/partyForm/loc/en-us.js
@@ -1,7 +1,7 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field"
+    "PropertyPaneDescription": "Form for adding and editing Parties",
+    "BasicGroupName": "Settings",
+    "ApiUrlFieldLabel": "Parties API URL"
   }
 });
