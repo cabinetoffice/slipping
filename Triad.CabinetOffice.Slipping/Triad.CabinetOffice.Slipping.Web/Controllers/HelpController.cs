f838cafb067a0cf29860e1604954791b700934c6
andrew-lott <andrewlott.uk@gmail.com>
2017-11-14 10:46:17 +0000

    PAWS2-298 - Added help controller and cookie description page (to match gov.uk path /help/cookies). /help currently forwards to cookie page.

---
+++ Triad.CabinetOffice.Slipping/Triad.CabinetOffice.Slipping.Web/Controllers/HelpController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Triad.CabinetOffice.Slipping.Web.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            return View("Cookies");
+        }
+
+        // GET: Cookies
+        public ActionResult Cookies()
+        {
+            return View();
+        }
+    }
+}
