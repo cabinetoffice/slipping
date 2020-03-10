4d459e6a1d090be290eb8a05b2c25d24b38b2408
Steve Brockton <stevebrockton@yahoo.co.uk>
2020-03-10 15:28:14 +0000

    PBI4545: Health check slipping app

---
+++ Triad.CabinetOffice.Slipping/Triad.CabinetOffice.Slipping.Data/Repositories/HealthCheckRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Triad.CabinetOffice.Slipping.Data.EntityFramework.Slipping;
+
+namespace Triad.CabinetOffice.Slipping.Data.Repositories
+{
+    public class HealthCheckRepository : RepositoryBase
+    {
+        public HealthCheckRepository() : base()
+        {
+        }
+
+        public HealthCheckRepository(SlippingEntities slippingContext) : base(slippingContext)
+        {
+        }
+        
+        /// <summary>
+        /// Check we can access the database 
+        /// (just read the first row from the MembersOfParliament table)
+        /// </summary>
+        /// <returns>true if database is accessible</returns>
+        public bool HealthCheck()
+        {
+            bool dbAccessible = false;
+            try
+            {
+                MembersOfParliament mp = db.MembersOfParliaments.FirstOrDefault();
+                dbAccessible = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return dbAccessible;
+        }
+    }
+}
---
+++ Triad.CabinetOffice.Slipping/Triad.CabinetOffice.Slipping.Web/Controllers/HealthCheckController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Triad.CabinetOffice.Slipping.Data.EntityFramework.Slipping;
+using Triad.CabinetOffice.Slipping.Data.Repositories;
+
+namespace Triad.CabinetOffice.Slipping.Web.Controllers
+{
+    public class HealthCheckController : Controller
+    {
+        #region Properties
+        private HealthCheckRepository healthCheckRepository { get { return new HealthCheckRepository(); } }
+        #endregion Properties
+
+        #region Methods
+        /// <summary>
+        /// Check that the database is accessible
+        /// </summary>
+        /// <returns>http status 200 if the check succeeds, else http status 500</returns>
+        public HttpStatusCodeResult Index()
+        {
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            try
+            {
+                if (healthCheckRepository.HealthCheck())
+                {
+                    statusCode = (int)HttpStatusCode.OK;
+                }
+            }
+            catch (Exception)
+            {
+                statusCode = (int)HttpStatusCode.InternalServerError;
+            }
+            return new HttpStatusCodeResult(statusCode);
+        }
+
+        #endregion Methods
+    }
+}
